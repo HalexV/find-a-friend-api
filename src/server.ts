daca005c54c1e0305c9ea1dde3357667e344a7b2
Halex Viotto Gomes <halexx002@gmail.com>
2023-05-24 10:31:59 -0400

    feat: load env vars with dotenv and zod

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
 import { app } from './app';
+import { env } from './env';
 
 app
   .listen({
     host: '0.0.0.0',
-    port: 3000,
+    port: env.PORT,
   })
   .then(() => {
     console.log('Server Running!');
