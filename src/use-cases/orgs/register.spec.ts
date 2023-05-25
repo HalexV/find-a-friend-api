a99c0c9e668e294212841b5443b8376f894469c1
Halex Viotto Gomes <halexx002@gmail.com>
2023-05-25 15:59:14 -0400

    test: not be able register twice same email

--- src/use-cases/orgs/register.spec.ts
+++ src/use-cases/orgs/register.spec.ts
@@ -2,6 +2,7 @@ import { expect, describe, it, beforeEach } from 'vitest';
 import { RegisterUseCase } from './register';
 import { InMemoryOrgsRepository } from '@/repositories/in-memory/in-memory-orgs-repository';
 import argon2 from 'argon2';
+import { OrgAlreadyExistsError } from '../errors/org-already-exists-error';
 
 let orgsRepository: InMemoryOrgsRepository;
 let sut: RegisterUseCase;
@@ -50,5 +51,35 @@ describe('Orgs - Register Use Case', () => {
 
     expect('12345678').not.toEqual(org.password_hash);
     expect(isPasswordCorrectlyHashed).toBeTruthy();
+  });
+
+  it('should not be able to register with same email twice', async () => {
+    await sut.execute({
+      address: 'Avenida das nações nº 4040',
+      cep: '99999000',
+      city: 'Colorado',
+      email: 'org@org.com',
+      latitude: -12.7569858,
+      longitude: -60.1626287,
+      name: 'Org Adote',
+      password: '12345678',
+      responsible_name: 'John Doe',
+      whatsapp_number: '+55099911223344',
+    });
+
+    const promise = sut.execute({
+      address: 'Avenida das nações nº 4040',
+      cep: '99999000',
+      city: 'Colorado',
+      email: 'org@org.com',
+      latitude: -12.7569858,
+      longitude: -60.1626287,
+      name: 'Org Adote',
+      password: '12345678',
+      responsible_name: 'John Doe',
+      whatsapp_number: '+55099911223344',
+    });
+
+    await expect(promise).rejects.toBeInstanceOf(OrgAlreadyExistsError);
   });
 });
