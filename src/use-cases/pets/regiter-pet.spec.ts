71c3b65e5e424a72d4353dcc720bf95f8c53de99
Halex Viotto Gomes <halexx002@gmail.com>
2023-06-01 15:54:43 -0400

    test: not be able to register a pet when adoption requirement not exist

--- src/use-cases/pets/regiter-pet.spec.ts
+++ src/use-cases/pets/regiter-pet.spec.ts
@@ -2,14 +2,21 @@ import { expect, describe, it, beforeEach } from 'vitest';
 import { RegisterPetUseCase } from './register-pet';
 import { InMemoryOrgsRepository } from '@/repositories/in-memory/in-memory-orgs-repository';
 import { ResourceNotFoundError } from '../errors/resource-not-found-error';
+import { InMemoryAdoptionRequirementsRepository } from '@/repositories/in-memory/in-memory-adoption-requirements-repository';
 
 let orgsRepository: InMemoryOrgsRepository;
+let adoptionRequirementsRepository: InMemoryAdoptionRequirementsRepository;
 let sut: RegisterPetUseCase;
 
 describe('Pets - Register Use Case', () => {
   beforeEach(() => {
     orgsRepository = new InMemoryOrgsRepository();
-    sut = new RegisterPetUseCase(orgsRepository);
+    adoptionRequirementsRepository =
+      new InMemoryAdoptionRequirementsRepository();
+    sut = new RegisterPetUseCase(
+      orgsRepository,
+      adoptionRequirementsRepository
+    );
   });
 
   it('should not be able to register a pet when org does not exist', async () => {
@@ -23,6 +30,42 @@ describe('Pets - Register Use Case', () => {
       independenceLevel: 'HIGH',
       name: 'Mark',
       orgId: 'non-existent-id',
+      size: 'MEDIUM',
+      type: 'DOG',
+    });
+
+    await expect(promise).rejects.toBeInstanceOf(ResourceNotFoundError);
+  });
+
+  it('should not be able to register a pet when some adoption requirement does not exist', async () => {
+    const org = await orgsRepository.create({
+      address: 'Avenida das nações nº 4040',
+      cep: '99999000',
+      city: 'Colorado',
+      email: 'org@org.com',
+      latitude: -12.7569858,
+      longitude: -60.1626287,
+      name: 'Org Adote',
+      password_hash: '12345678',
+      responsible_name: 'John Doe',
+      whatsapp_number: '+55099911223344',
+    });
+
+    const adoptionRequirement = await adoptionRequirementsRepository.create({
+      org_id: org.id,
+      title: 'Cold weather',
+    });
+
+    const promise = sut.execute({
+      about: 'any',
+      adoptionRequirementsIds: ['non-existent-id', adoptionRequirement.id],
+      age: 'PUPPY',
+      ambience: 'MEDIUM',
+      available: true,
+      energyLevel: 'AVERAGE',
+      independenceLevel: 'HIGH',
+      name: 'Mark',
+      orgId: org.id,
       size: 'MEDIUM',
       type: 'DOG',
     });
