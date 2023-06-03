cc2a7c3ed952016f310cf169da3aadb429e3eec2
Halex Viotto Gomes <halexx002@gmail.com>
2023-06-03 19:46:00 -0400

    test: should be able to create pet with adoption requirements

--- src/use-cases/pets/register-pet.spec.ts
+++ src/use-cases/pets/register-pet.spec.ts
@@ -136,4 +136,70 @@ describe('Pets - Register Use Case', () => {
 
     expect(pet).toMatchObject(expectedPet);
   });
+
+  it('should be able to create a pet with adoption requirements', async () => {
+    const createdAt = new Date('2023-01-01');
+
+    vi.setSystemTime(createdAt);
+
+    const org = await orgsRepository.create({
+      address: 'Avenida das nações nº 4040',
+      cep: '99999000',
+      city: 'Colorado',
+      email: 'org@org.com',
+      latitude: -12.7569858,
+      longitude: -60.1626287,
+      name: 'Org Adote',
+      password_hash: '12345678',
+      responsible_name: 'John Doe',
+      whatsapp_number: '+55099911223344',
+    });
+
+    const adoptionRequirementA = await adoptionRequirementsRepository.create({
+      org_id: org.id,
+      title: 'Cold weather',
+    });
+
+    const adoptionRequirementB = await adoptionRequirementsRepository.create({
+      org_id: org.id,
+      title: 'It needs a yard',
+    });
+
+    await sut.execute({
+      about: 'any',
+      adoptionRequirementsIds: [
+        adoptionRequirementA.id,
+        adoptionRequirementB.id,
+      ],
+      age: 'PUPPY',
+      ambience: 'MEDIUM',
+      available: true,
+      energyLevel: 'AVERAGE',
+      independenceLevel: 'HIGH',
+      name: 'Mark',
+      orgId: org.id,
+      size: 'MEDIUM',
+      type: 'DOG',
+    });
+
+    const expectedPet = {
+      about: 'any',
+      adoption_requirements: [adoptionRequirementA.id, adoptionRequirementB.id],
+      age: 'PUPPY',
+      ambience: 'MEDIUM',
+      available: true,
+      energy_level: 'AVERAGE',
+      independence_level: 'HIGH',
+      name: 'Mark',
+      org_id: org.id,
+      size: 'MEDIUM',
+      type: 'DOG',
+      created_at: createdAt,
+      updated_at: createdAt,
+    };
+
+    const pet = petsRepository.items[0];
+
+    expect(pet).toMatchObject(expectedPet);
+  });
 });
