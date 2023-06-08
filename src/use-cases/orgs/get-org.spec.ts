494fe7670b8244f84a111717290fc8b0fe19e58a
Halex Viotto Gomes <halexx002@gmail.com>
2023-06-08 16:47:10 -0400

    test: should be able to get an org

--- src/use-cases/orgs/get-org.spec.ts
+++ src/use-cases/orgs/get-org.spec.ts
@@ -2,6 +2,7 @@ import { expect, describe, it, beforeEach } from 'vitest';
 import { InMemoryOrgsRepository } from '@/repositories/in-memory/in-memory-orgs-repository';
 import { GetOrgUseCase } from './get-org';
 import { ResourceNotFoundError } from '../errors/resource-not-found-error';
+import argon2 from 'argon2';
 
 let orgsRepository: InMemoryOrgsRepository;
 let sut: GetOrgUseCase;
@@ -12,7 +13,26 @@ describe('Orgs - Get Org Use Case', () => {
     sut = new GetOrgUseCase(orgsRepository);
   });
 
-  it('should not be able to get a org with a non-existent id', async () => {
+  it('should be able to get an org', async () => {
+    const orgCreated = await orgsRepository.create({
+      address: 'Avenida das nações nº 4040',
+      cep: '99999000',
+      city: 'Colorado',
+      email: 'org@org.com',
+      latitude: -12.7569858,
+      longitude: -60.1626287,
+      name: 'Org Adote',
+      password_hash: await argon2.hash('12345678'),
+      responsible_name: 'John Doe',
+      whatsapp_number: '+55099911223344',
+    });
+
+    const { org } = await sut.execute({ orgId: orgCreated.id });
+
+    expect(org).toEqual(orgCreated);
+  });
+
+  it('should not be able to get an org with a non-existent id', async () => {
     const promise = sut.execute({
       orgId: 'non-existent-id',
     });
