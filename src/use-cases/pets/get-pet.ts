ba816d30b1b4e905e4ef520affc75750b6613ba7
Halex Viotto Gomes <halexx002@gmail.com>
2023-06-08 16:25:29 -0400

    feat: should be able to get a pet

--- src/use-cases/pets/get-pet.ts
+++ src/use-cases/pets/get-pet.ts
@@ -1,23 +1,39 @@
 import { PetComplete } from './register-pet';
 import { ResourceNotFoundError } from '../errors/resource-not-found-error';
 import { PetsRepository } from '@/repositories/pets-repository';
+import { PhotosRepository } from '@/repositories/photos-repository';
 
 interface GetPetUseCaseRequest {
   petId: string;
 }
 
 interface GetPetUseCaseResponse {
-  pets: PetComplete;
+  pet: PetComplete;
 }
 
 export class GetPetUseCase {
-  constructor(private petsRepository: PetsRepository) {}
+  constructor(
+    private petsRepository: PetsRepository,
+    private photosRepository: PhotosRepository
+  ) {}
 
-  async execute({ petId }: GetPetUseCaseRequest): Promise<void> {
+  async execute({
+    petId,
+  }: GetPetUseCaseRequest): Promise<GetPetUseCaseResponse> {
     const pet = await this.petsRepository.findById(petId);
 
     if (!pet) {
       throw new ResourceNotFoundError();
     }
+
+    const photos = await this.photosRepository.findManyByPetId(pet.id);
+
+    const completedPet = Object.assign({}, pet, {
+      photos: photos.map((photo) => photo.url),
+    });
+
+    return {
+      pet: completedPet,
+    };
   }
 }
