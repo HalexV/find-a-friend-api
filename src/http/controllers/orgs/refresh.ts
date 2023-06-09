0250a2a115437bfb90a0803745f7d92bf27f36ec
Halex Viotto Gomes <halexx002@gmail.com>
2023-06-09 16:34:07 -0400

    feat: should be able to refresh a token

---
+++ src/http/controllers/orgs/refresh.ts
@@ -0,0 +1,37 @@
+import { FastifyReply, FastifyRequest } from 'fastify';
+
+export async function refresh(request: FastifyRequest, reply: FastifyReply) {
+  // Olha apenas para os cabeçalhos de cookies procurando o refresh token
+  await request.jwtVerify({ onlyCookie: true });
+
+  const token = await reply.jwtSign(
+    {},
+    {
+      sign: {
+        sub: request.user.sub,
+      },
+    }
+  );
+
+  const refreshToken = await reply.jwtSign(
+    {},
+    {
+      sign: {
+        sub: request.user.sub,
+        expiresIn: '7d',
+      },
+    }
+  );
+
+  return reply
+    .setCookie('refreshToken', refreshToken, {
+      path: '/',
+      secure: true,
+      sameSite: true,
+      httpOnly: true,
+    })
+    .status(200)
+    .send({
+      token,
+    });
+}
--- src/http/controllers/orgs/routes.ts
+++ src/http/controllers/orgs/routes.ts
@@ -1,8 +1,11 @@
 import { FastifyInstance } from 'fastify';
 import { register } from './register';
 import { authenticate } from './authenticate';
+import { refresh } from './refresh';
 
 export async function orgRoutes(app: FastifyInstance) {
   app.post('/orgs', register);
   app.post('/sessions', authenticate);
+
+  app.patch('/token/refresh', refresh);
 }
