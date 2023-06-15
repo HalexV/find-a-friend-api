2255f2c1545c1bac0661f1f812ab84e3698e37a9
Halex Viotto Gomes <halexx002@gmail.com>
2023-06-15 16:38:48 -0400

    test: should be able to remove a pet

--- src/use-cases/pets/remove-pet.spec.ts
+++ src/use-cases/pets/remove-pet.spec.ts
@@ -3,7 +3,9 @@ import { ResourceNotFoundError } from '../errors/resource-not-found-error';
 import { InMemoryAdoptionRequirementsRepository } from '@/repositories/in-memory/in-memory-adoption-requirements-repository';
 import { InMemoryPetsRepository } from '@/repositories/in-memory/in-memory-pets-repository';
 import { RemovePetUseCase } from './remove-pet';
+import { InMemoryOrgsRepository } from '@/repositories/in-memory/in-memory-orgs-repository';
 
+let orgsRepository: InMemoryOrgsRepository;
 let adoptionRequirementsRepository: InMemoryAdoptionRequirementsRepository;
 let petsRepository: InMemoryPetsRepository;
 let sut: RemovePetUseCase;
@@ -15,6 +17,8 @@ describe('Pets - Remove Pet Use Case', () => {
     petsRepository = new InMemoryPetsRepository(adoptionRequirementsRepository);
 
     sut = new RemovePetUseCase(petsRepository);
+
+    orgsRepository = new InMemoryOrgsRepository();
   });
 
   it('should not be able to remove a pet when pet does not exist', async () => {
@@ -23,5 +27,38 @@ describe('Pets - Remove Pet Use Case', () => {
     });
 
     await expect(promise).rejects.toBeInstanceOf(ResourceNotFoundError);
+  });
+
+  it('should be able to remove a pet', async () => {
+    const org = await orgsRepository.create({
+      address: 'Avenida das nações nº 4040',
+      cep: '99999000',
+      city: 'Colorado',
+      email: 'org@org.com',
+      latitude: -12.7569858,
+      longitude: -60.1626287,
+      name: 'Org Adote',
+      password_hash: '12345678',
+      responsible_name: 'John Doe',
+      whatsapp_number: '+55099911223344',
+    });
+
+    const pet = await petsRepository.create({
+      about: 'any',
+      adoption_requirements: [],
+      age: 'PUPPY',
+      ambience: 'MEDIUM',
+      available: true,
+      energy_level: 'AVERAGE',
+      independence_level: 'HIGH',
+      name: 'Mark',
+      org_id: org.id,
+      size: 'MEDIUM',
+      type: 'DOG',
+    });
+
+    await sut.execute({ petId: pet.id });
+
+    expect(petsRepository.items.length).toBe(0);
   });
 });
