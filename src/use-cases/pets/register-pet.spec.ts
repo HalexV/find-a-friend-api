4bda0519980603fe078edc6a2a8bbd81396605b5
Halex Viotto Gomes <halexx002@gmail.com>
2023-06-15 14:30:44 -0400

    refactor: remove photo registration from use case

--- src/use-cases/pets/register-pet.spec.ts
+++ src/use-cases/pets/register-pet.spec.ts
@@ -1,31 +1,13 @@
-import {
-  expect,
-  describe,
-  it,
-  beforeEach,
-  vi,
-  afterEach,
-  afterAll,
-} from 'vitest';
+import { expect, describe, it, beforeEach, vi, afterEach } from 'vitest';
 import { RegisterPetUseCase } from './register-pet';
 import { InMemoryOrgsRepository } from '@/repositories/in-memory/in-memory-orgs-repository';
 import { ResourceNotFoundError } from '../errors/resource-not-found-error';
 import { InMemoryAdoptionRequirementsRepository } from '@/repositories/in-memory/in-memory-adoption-requirements-repository';
 import { InMemoryPetsRepository } from '@/repositories/in-memory/in-memory-pets-repository';
 
-import { open } from 'node:fs/promises';
-import { join } from 'node:path';
-import { Readable } from 'node:stream';
-import { InMemoryPhotosRepository } from '@/repositories/in-memory/in-memory-photos-repository';
-
-const basePath = join(__dirname, '../../utils/test/images');
-
-const streams: Readable[] = [];
-
 let orgsRepository: InMemoryOrgsRepository;
 let adoptionRequirementsRepository: InMemoryAdoptionRequirementsRepository;
 let petsRepository: InMemoryPetsRepository;
-let photosRepository: InMemoryPhotosRepository;
 let sut: RegisterPetUseCase;
 
 describe('Pets - Register Use Case', () => {
@@ -34,13 +16,11 @@ describe('Pets - Register Use Case', () => {
     adoptionRequirementsRepository =
       new InMemoryAdoptionRequirementsRepository();
     petsRepository = new InMemoryPetsRepository(adoptionRequirementsRepository);
-    photosRepository = new InMemoryPhotosRepository();
 
     sut = new RegisterPetUseCase(
       orgsRepository,
       adoptionRequirementsRepository,
-      petsRepository,
-      photosRepository
+      petsRepository
     );
 
     vi.useFakeTimers();
@@ -49,10 +29,6 @@ describe('Pets - Register Use Case', () => {
 
   afterEach(() => {
     vi.useRealTimers();
-  });
-
-  afterAll(() => {
-    streams.forEach((stream) => stream.destroy());
   });
 
   it('should not be able to register a pet when org does not exist', async () => {
@@ -68,7 +44,6 @@ describe('Pets - Register Use Case', () => {
       orgId: 'non-existent-id',
       size: 'MEDIUM',
       type: 'DOG',
-      photos: [],
     });
 
     await expect(promise).rejects.toBeInstanceOf(ResourceNotFoundError);
@@ -105,7 +80,6 @@ describe('Pets - Register Use Case', () => {
       orgId: org.id,
       size: 'MEDIUM',
       type: 'DOG',
-      photos: [],
     });
 
     await expect(promise).rejects.toBeInstanceOf(ResourceNotFoundError);
@@ -141,7 +115,6 @@ describe('Pets - Register Use Case', () => {
       orgId: org.id,
       size: 'MEDIUM',
       type: 'DOG',
-      photos: [],
     });
 
     const expectedPet = {
@@ -208,7 +181,6 @@ describe('Pets - Register Use Case', () => {
       orgId: org.id,
       size: 'MEDIUM',
       type: 'DOG',
-      photos: [],
     });
 
     const expectedPet = {
@@ -230,141 +202,5 @@ describe('Pets - Register Use Case', () => {
     const pet = petsRepository.items[0];
 
     expect(pet).toMatchObject(expectedPet);
-  });
-
-  it('should be able to create a pet with photos', async () => {
-    const createdAt = new Date('2023-01-01');
-
-    vi.setSystemTime(createdAt);
-
-    const fd1 = await open(join(basePath, 'image1.jpg'));
-    const fd2 = await open(join(basePath, 'image2.jpg'));
-
-    const stream1 = fd1.createReadStream();
-    const stream2 = fd2.createReadStream();
-
-    streams.push(stream1, stream2);
-
-    const org = await orgsRepository.create({
-      address: 'Avenida das nações nº 4040',
-      cep: '99999000',
-      city: 'Colorado',
-      email: 'org@org.com',
-      latitude: -12.7569858,
-      longitude: -60.1626287,
-      name: 'Org Adote',
-      password_hash: '12345678',
-      responsible_name: 'John Doe',
-      whatsapp_number: '+55099911223344',
-    });
-
-    const { pet } = await sut.execute({
-      about: 'any',
-      adoptionRequirementsIds: [],
-      age: 'PUPPY',
-      ambience: 'MEDIUM',
-      available: true,
-      energyLevel: 'AVERAGE',
-      independenceLevel: 'HIGH',
-      name: 'Mark',
-      orgId: org.id,
-      size: 'MEDIUM',
-      type: 'DOG',
-      photos: [
-        { file: stream1, type: 'JPEG' },
-        { file: stream2, type: 'JPEG' },
-      ],
-    });
-
-    const expectedPet = {
-      about: 'any',
-      adoption_requirements: [],
-      age: 'PUPPY',
-      ambience: 'MEDIUM',
-      available: true,
-      energy_level: 'AVERAGE',
-      independence_level: 'HIGH',
-      name: 'Mark',
-      org_id: org.id,
-      size: 'MEDIUM',
-      type: 'DOG',
-      photos: [expect.any(String), expect.any(String)],
-      created_at: createdAt,
-      updated_at: createdAt,
-    };
-
-    expect(pet).toMatchObject(expectedPet);
-  });
-
-  it('should be able to revert pet register when photo registration has errors', async () => {
-    const createdAt = new Date('2023-01-01');
-
-    vi.setSystemTime(createdAt);
-
-    const photoCreate = photosRepository.create.bind(photosRepository);
-
-    vi.spyOn(photosRepository, 'create')
-      .mockImplementationOnce(photoCreate)
-      .mockImplementationOnce(() => {
-        throw new Error();
-      });
-
-    const fd1 = await open(join(basePath, 'image1.jpg'));
-    const fd2 = await open(join(basePath, 'image2.jpg'));
-
-    const stream1 = fd1.createReadStream();
-    const stream2 = fd2.createReadStream();
-
-    streams.push(stream1, stream2);
-
-    const org = await orgsRepository.create({
-      address: 'Avenida das nações nº 4040',
-      cep: '99999000',
-      city: 'Colorado',
-      email: 'org@org.com',
-      latitude: -12.7569858,
-      longitude: -60.1626287,
-      name: 'Org Adote',
-      password_hash: '12345678',
-      responsible_name: 'John Doe',
-      whatsapp_number: '+55099911223344',
-    });
-
-    const adoptionRequirementA = await adoptionRequirementsRepository.create({
-      org_id: org.id,
-      title: 'Cold weather',
-    });
-
-    const adoptionRequirementB = await adoptionRequirementsRepository.create({
-      org_id: org.id,
-      title: 'It needs a yard',
-    });
-
-    const promise = sut.execute({
-      about: 'any',
-      adoptionRequirementsIds: [
-        adoptionRequirementA.id,
-        adoptionRequirementB.id,
-      ],
-      age: 'PUPPY',
-      ambience: 'MEDIUM',
-      available: true,
-      energyLevel: 'AVERAGE',
-      independenceLevel: 'HIGH',
-      name: 'Mark',
-      orgId: org.id,
-      size: 'MEDIUM',
-      type: 'DOG',
-      photos: [
-        { file: stream1, type: 'JPEG' },
-        { file: stream2, type: 'JPEG' },
-      ],
-    });
-
-    await expect(promise).rejects.toThrowError();
-    expect(photosRepository.items.length).toBe(0);
-    expect(petsRepository.items.length).toBe(0);
-    expect(adoptionRequirementsRepository.items[0].pets.length).toBe(0);
-    expect(adoptionRequirementsRepository.items[1].pets.length).toBe(0);
   });
 });
--- src/use-cases/pets/register-pet.ts
+++ src/use-cases/pets/register-pet.ts
@@ -4,7 +4,6 @@ import {
   Ambience,
   EnergyLevel,
   IndependenceLevel,
-  Pet,
   Size,
   Type,
 } from '@prisma/client';
@@ -14,14 +13,6 @@ import {
   PetWithAdoptionRequirements,
   PetsRepository,
 } from '@/repositories/pets-repository';
-import { Readable } from 'node:stream';
-import { PhotosRepository } from '@/repositories/photos-repository';
-// import { prisma } from '@/lib/prisma';
-
-interface PhotoStream {
-  file: Readable;
-  type: 'JPEG';
-}
 
 interface RegisterPetUseCaseRequest {
   about: string;
@@ -34,25 +25,18 @@ interface RegisterPetUseCaseRequest {
   size: Size;
   type: Type;
   orgId: string;
-  photos: PhotoStream[];
   adoptionRequirementsIds: string[];
 }
 
-export interface PetComplete extends Pet {
-  adoption_requirements: string[];
-  photos: string[];
-}
-
 interface RegisterPetUseCaseResponse {
-  pet: PetComplete;
+  pet: PetWithAdoptionRequirements;
 }
 
 export class RegisterPetUseCase {
   constructor(
     private orgsRepository: OrgsRepository,
     private adoptionRequirementsRepository: AdoptionRequirementsRepository,
-    private petsRepository: PetsRepository,
-    private photosRepository: PhotosRepository
+    private petsRepository: PetsRepository
   ) {}
 
   async execute({
@@ -66,7 +50,6 @@ export class RegisterPetUseCase {
     size,
     type,
     orgId,
-    photos,
     adoptionRequirementsIds,
   }: RegisterPetUseCaseRequest): Promise<RegisterPetUseCaseResponse> {
     const org = await this.orgsRepository.findById(orgId);
@@ -94,33 +77,6 @@ export class RegisterPetUseCase {
       adoption_requirements: adoptionRequirementsIds,
     });
 
-    const petWithAdoptionRequirementsAndPhotos = Object.assign({}, pet, {
-      photos: [] as string[],
-    });
-
-    const photoIds = [];
-
-    try {
-      for (const photoObject of photos) {
-        const photo = await this.photosRepository.create({
-          file: photoObject.file,
-          petId: pet.id,
-        });
-
-        photoIds.push(photo.id);
-
-        petWithAdoptionRequirementsAndPhotos.photos.push(photo.url);
-      }
-    } catch (error) {
-      for (const id of photoIds) {
-        await this.photosRepository.delete(id);
-      }
-
-      await this.petsRepository.delete(pet.id);
-
-      throw error;
-    }
-
-    return { pet: petWithAdoptionRequirementsAndPhotos };
+    return { pet };
   }
 }
