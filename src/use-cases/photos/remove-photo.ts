93fa75e415b45103d4132271c6afa39d4406bd47
Halex Viotto Gomes <halexx002@gmail.com>
2023-06-15 16:53:33 -0400

    feat: not be able to remove a pet photo when photo not exist

---
+++ src/use-cases/photos/remove-photo.ts
@@ -0,0 +1,18 @@
+import { PhotosRepository } from '@/repositories/photos-repository';
+import { ResourceNotFoundError } from '../errors/resource-not-found-error';
+
+interface RemovePhotoUseCaseRequest {
+  photoId: string;
+}
+
+export class RemovePhotoUseCase {
+  constructor(private photosRepository: PhotosRepository) {}
+
+  async execute({ photoId }: RemovePhotoUseCaseRequest): Promise<void> {
+    const photo = await this.photosRepository.findById(photoId);
+
+    if (!photo) {
+      throw new ResourceNotFoundError();
+    }
+  }
+}
