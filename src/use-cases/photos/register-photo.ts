c10c507fbe1c8a78019d8ab41218cfd970fa5e91
Halex Viotto Gomes <halexx002@gmail.com>
2023-06-15 13:55:36 -0400

    feat: it should be able to register a pet photo

---
+++ src/use-cases/photos/register-photo.ts
@@ -0,0 +1,44 @@
+import { Photo } from '@prisma/client';
+import { Readable } from 'node:stream';
+import { PhotosRepository } from '@/repositories/photos-repository';
+import { PetsRepository } from '@/repositories/pets-repository';
+import { ResourceNotFoundError } from '../errors/resource-not-found-error';
+
+interface PhotoStream {
+  file: Readable;
+  type: 'JPEG';
+}
+
+interface RegisterPhotoUseCaseRequest {
+  petId: string;
+  photo: PhotoStream;
+}
+
+interface RegisterPhotoUseCaseResponse {
+  photo: Photo;
+}
+
+export class RegisterPhotoUseCase {
+  constructor(
+    private photosRepository: PhotosRepository,
+    private petsRepository: PetsRepository
+  ) {}
+
+  async execute({
+    petId,
+    photo,
+  }: RegisterPhotoUseCaseRequest): Promise<RegisterPhotoUseCaseResponse> {
+    const pet = await this.petsRepository.findById(petId);
+
+    if (!pet) {
+      throw new ResourceNotFoundError();
+    }
+
+    const photoCreated = await this.photosRepository.create({
+      file: photo.file,
+      petId,
+    });
+
+    return { photo: photoCreated };
+  }
+}
