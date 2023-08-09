39306916351fb243f4503ff783a46194fbd9615c
Halex Viotto Gomes <halexx002@gmail.com>
2023-08-09 16:14:11 -0400

    chore: add coverage exclude list

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
-import { defineConfig } from 'vitest/config';
+import { defineConfig, configDefaults } from 'vitest/config';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
   test: {
     environmentMatchGlobs: [['src/http/controllers/**', 'prisma']],
+    coverage: {
+      exclude: [
+        'src/**/*.spec.ts',
+        'src/env',
+        'src/utils/test',
+        'prisma',
+        'src/lib',
+      ],
+    },
   },
 });
